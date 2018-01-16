881fe70532a2acf874a0b14f00c9a7a739fd37ab
Xander Apponi <xapponi@gmail.com>
2018-01-15 21:27:28 -0600

    Fixed dot.c

--- dot.c
+++ dot.c
@@ -1,7 +1,7 @@
 double dot(double a[], double b[], int length) {
 	int i;
 	double sum;   
-	for (i=1; i<= length; i++) { 
+	for (i=0; i<length; i++) {       //set i=0 and use <, not <=
 		sum = sum + a[i]*b[i];
 		}
 	return sum;
