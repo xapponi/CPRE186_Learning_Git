daeea8f4653175445c091ff6d477d2ebd97b1890
Xander Apponi <xapponi@gmail.com>
2018-01-15 21:39:48 -0600

    initialized variable 'sum' to 0 before it calculated the dot product

--- dot.c
+++ dot.c
@@ -1,6 +1,6 @@
 double dot(double a[], double b[], int length) {
 	int i;
-	double sum;   
+	double sum = 0;   
 	for (i=0; i<length; i++) {       //set i=0 and use <, not <=
 		sum = sum + a[i]*b[i];
 		}
